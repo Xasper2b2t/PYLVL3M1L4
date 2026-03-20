74946d56dcc043dd06007a9e3d2111a0a8f621e2
Xasper2b2t <iiodklyou.were@gmail.com>
2026-03-20 22:14:51 +0300

    !2

--- logic.py
+++ logic.py
@@ -4,14 +4,26 @@ import requests
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer):
-
+    def __init__(self, name, image, type_, hp, attack, defense, speed, abilities, pokemon_trainer):
+        self.name = name
+        self.image = image
+        self.type_ = type_
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.abilities = abilities
         self.pokemon_trainer = pokemon_trainer   
 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+        self.type_=self.get_type()
+        self.hp=self.get_hp()
+        self.attack=self.get_attack()
+        self.deffense=self.get_deffense()
+        self.speed=self.get_speed()
+        self.abilities=self.get_abilities()
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -32,7 +44,54 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-
+    def get_type(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['types'])
+        else:
+            return "Pikachu"
+    def get_hp(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['stats']['stat']['name']['hp'])
+        else:
+            return "Pikachu"
+    def get_attack(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['stats']['stat']['name']['attack'])
+        else:
+            return "Pikachu"
+    def get_deffense(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['stats']['stat']['name']['deffense'])
+        else:
+            return "Pikachu"
+    def get_speed(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['stats']['stat']['name']['speed'])
+        else:
+            return "Pikachu"
+    def get_abilities(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities']['ability']['name'])
+        else:
+            return "Pikachu"
 
     # Метод класса для получения информации
     def info(self):
